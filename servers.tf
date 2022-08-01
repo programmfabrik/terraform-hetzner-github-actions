aefa63972fc7eb6bd8451ad0d8b5a9c86075a995
leonsteinhaeuser <leonsteinhaeuser@gmx.de>
2022-08-01 17:25:12 +0200

    fix: terraform formatting

--- servers.tf
+++ servers.tf
@@ -1,10 +1,10 @@
 locals {
-  fill_char_length = 64-length("github-runner")-4
+  fill_char_length = 64 - length("github-runner") - 4
 }
 
 resource "random_string" "hetzner_machine" {
-  length           = local.fill_char_length > 0 ? local.fill_char_length : 0
-  special          = false
+  length  = local.fill_char_length > 0 ? local.fill_char_length : 0
+  special = false
 }
 
 resource "hcloud_server" "github_runner" {
@@ -33,42 +33,42 @@ resource "hcloud_server" "github_runner" {
 
   provisioner "remote-exec" {
     inline = [
-        "apt-get update -y", 
-        "DEBIAN_FRONTEND=noninteractive apt-get upgrade -y", 
-        "DEBIAN_FRONTEND=noninteractive apt-get install sudo git vim tmux apt-transport-https ca-certificates curl gnupg lsb-release pass ${var.hetzner_machine_additional_packages} -y",
-        "echo '127.0.0.1       fylr-server-postgres    fylr-server-sqlite      execserver      minio2  postgres2       elasticsearch2' >> /etc/hosts",
-        "curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg",
-        "echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian buster stable' | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
-        "apt-get update -y",
-        "apt-get install docker-ce docker-ce-cli containerd.io docker-compose -y",
-        "curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -",
-        "apt install nodejs",
-        "mkdir /srv/actions-runner && cd /srv/actions-runner",
-        "curl -o actions-runner-linux-x64-2.277.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.277.1/actions-runner-linux-x64-2.277.1.tar.gz",
-        "tar xzf ./actions-runner-linux-x64-2.277.1.tar.gz",
-        "adduser github-runner --disabled-login --gecos ''",
-        "usermod -aG docker github-runner",
-        "echo 'github-runner   ALL=(ALL:ALL)NOPASSWD:ALL' > /etc/sudoers.d/github-runner",
-        "chown -R github-runner /srv",
-        "chmod +x /srv/setup-runner.sh /srv/gh-runner-cli",
-        "mv /srv/actions-runner/run.sh /srv/actions-runner/run.sh.old",
-        "su github-runner -c 'export CUSTOM_HOSTNAME=${self.name}; export GH_USERNAME=${var.github_authentication_user}; export GH_TOKEN=${var.github_authentication_token}; export GH_OWNER=${var.github_owner}; export GH_NAME=${var.github_repository_name}; export GH_LABELS=${var.github_actions_runner_labels}; export GH_REPLACE_RUNNERS=${var.github_actions_runner_replace_existing}; export GH_RUNNER_TYPE=${var.github_runner_type}; /srv/setup-runner.sh'"
-        ]
+      "apt-get update -y",
+      "DEBIAN_FRONTEND=noninteractive apt-get upgrade -y",
+      "DEBIAN_FRONTEND=noninteractive apt-get install sudo git vim tmux apt-transport-https ca-certificates curl gnupg lsb-release pass ${var.hetzner_machine_additional_packages} -y",
+      "echo '127.0.0.1       fylr-server-postgres    fylr-server-sqlite      execserver      minio2  postgres2       elasticsearch2' >> /etc/hosts",
+      "curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg",
+      "echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian buster stable' | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
+      "apt-get update -y",
+      "apt-get install docker-ce docker-ce-cli containerd.io docker-compose -y",
+      "curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -",
+      "apt install nodejs",
+      "mkdir /srv/actions-runner && cd /srv/actions-runner",
+      "curl -o actions-runner-linux-x64-2.277.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.277.1/actions-runner-linux-x64-2.277.1.tar.gz",
+      "tar xzf ./actions-runner-linux-x64-2.277.1.tar.gz",
+      "adduser github-runner --disabled-login --gecos ''",
+      "usermod -aG docker github-runner",
+      "echo 'github-runner   ALL=(ALL:ALL)NOPASSWD:ALL' > /etc/sudoers.d/github-runner",
+      "chown -R github-runner /srv",
+      "chmod +x /srv/setup-runner.sh /srv/gh-runner-cli",
+      "mv /srv/actions-runner/run.sh /srv/actions-runner/run.sh.old",
+      "su github-runner -c 'export CUSTOM_HOSTNAME=${self.name}; export GH_USERNAME=${var.github_authentication_user}; export GH_TOKEN=${var.github_authentication_token}; export GH_OWNER=${var.github_owner}; export GH_NAME=${var.github_repository_name}; export GH_LABELS=${var.github_actions_runner_labels}; export GH_REPLACE_RUNNERS=${var.github_actions_runner_replace_existing}; export GH_RUNNER_TYPE=${var.github_runner_type}; /srv/setup-runner.sh'"
+    ]
   }
 }
 
 resource "null_resource" "deprovision" {
   triggers = {
-    machine_names = join(",", tolist(hcloud_server.github_runner.*.name))
-    github_user = var.github_authentication_user
-    github_user_token = var.github_authentication_token
-    github_repo_name = var.github_repository_name
-    github_repo_owner = var.github_owner
+    machine_names      = join(",", tolist(hcloud_server.github_runner.*.name))
+    github_user        = var.github_authentication_user
+    github_user_token  = var.github_authentication_token
+    github_repo_name   = var.github_repository_name
+    github_repo_owner  = var.github_owner
     github_runner_type = var.github_runner_type
   }
 
   provisioner "local-exec" {
-      when = destroy
-      command = "./scripts/local/destroy_runner.sh ${self.triggers.machine_names} ${self.triggers.github_user} ${self.triggers.github_user_token} ${self.triggers.github_repo_name} ${self.triggers.github_repo_owner} ${self.triggers.github_runner_type}"
+    when    = destroy
+    command = "./scripts/local/destroy_runner.sh ${self.triggers.machine_names} ${self.triggers.github_user} ${self.triggers.github_user_token} ${self.triggers.github_repo_name} ${self.triggers.github_repo_owner} ${self.triggers.github_runner_type}"
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,113 +1,113 @@
 # Set the variable value in *.tfvars file
 # or using the -var="hcloud_token=..." CLI option
 variable "hcloud_token" {
-    description = "Hetzner Cloud API token"
-    type = string
+  description = "Hetzner Cloud API token"
+  type        = string
 }
 
 variable "ssh_private_key" {
-    description = "Defines the path to the location of the private key. The private key is used together with the public key to connect to the machine."
-    default     = "~/.ssh/id_rsa"
-    type = string
+  description = "Defines the path to the location of the private key. The private key is used together with the public key to connect to the machine."
+  default     = "~/.ssh/id_rsa"
+  type        = string
 }
 
 variable "ssh_public_key" {
-    description = "Public Key to authorized the access to the machines"
-    default     = "~/.ssh/id_rsa.pub"
-    type = string
+  description = "Public Key to authorized the access to the machines"
+  default     = "~/.ssh/id_rsa.pub"
+  type        = string
 }
 
 variable "ssh_username" {
-    description = "Username that should be used to connect to the nodes"
-    default = "root"
-    type = string
+  description = "Username that should be used to connect to the nodes"
+  default     = "root"
+  type        = string
 }
 
 variable "ssh_key_name" {
-    description = "Defines the name for the ssh key"
-    default = "admin_ssh_key"
-    type = string
+  description = "Defines the name for the ssh key"
+  default     = "admin_ssh_key"
+  type        = string
 }
 
 resource "hcloud_ssh_key" "admin_ssh_key" {
-    name       = var.ssh_key_name
-    public_key = file(var.ssh_public_key)
+  name       = var.ssh_key_name
+  public_key = file(var.ssh_public_key)
 }
 
 variable "hetzner_machine_type" {
-    description = "Sets the machine type to use."
-    default = "cx11"
-    type = string
+  description = "Sets the machine type to use."
+  default     = "cx11"
+  type        = string
 }
 
 variable "hetzner_machine_os" {
-    description = "Defines the machine operating system to be installed."
-    default = "debian-10"
-    type = string
+  description = "Defines the machine operating system to be installed."
+  default     = "debian-10"
+  type        = string
 }
 
-variable hetzner_additional_public_key_ids {
-    description = "Adds public keys to the server that are already registered at hetzner"
-    default = []
-    type = list(string)
+variable "hetzner_additional_public_key_ids" {
+  description = "Adds public keys to the server that are already registered at hetzner"
+  default     = []
+  type        = list(string)
 }
 
-variable hetzner_machine_additional_packages {
-    description = "Defines additional packages that must be installed on the machine. Each package name must be separated by a space ` `."
-    default = ""
-    type = string
+variable "hetzner_machine_additional_packages" {
+  description = "Defines additional packages that must be installed on the machine. Each package name must be separated by a space ` `."
+  default     = ""
+  type        = string
 }
 
-variable hetzner_machine_location {
-    description = "Specifies the location of the data center where the machine is to be deployed."
-    default = "nbg1"
-    type = string
+variable "hetzner_machine_location" {
+  description = "Specifies the location of the data center where the machine is to be deployed."
+  default     = "nbg1"
+  type        = string
 }
 
 //
 
 variable "github_actions_runner_count" {
-    description = "Defines the number of runners to be provided. This option is equal to Machines at hetzner."
-    default = 1
-    type = number
+  description = "Defines the number of runners to be provided. This option is equal to Machines at hetzner."
+  default     = 1
+  type        = number
 }
 
 variable "github_actions_runner_labels" {
-    description = "Defines a list of labels used to identify the runners. The list is divided by separating the individual entries with `,`."
-    default = ""
-    type = string
+  description = "Defines a list of labels used to identify the runners. The list is divided by separating the individual entries with `,`."
+  default     = ""
+  type        = string
 }
 
 variable "github_actions_runner_replace_existing" {
-    description = "Specifies whether to replace existing Github action runners with the same name."
-    default = false
-    type = bool
+  description = "Specifies whether to replace existing Github action runners with the same name."
+  default     = false
+  type        = bool
 }
 
 variable "github_owner" {
-    description = "Defines the organisation name or repository owner."
-    default = ""
-    type = string
+  description = "Defines the organisation name or repository owner."
+  default     = ""
+  type        = string
 }
 
 variable "github_repository_name" {
-    description = "Sets the name of the repository. This option is only used if you use self-hosted Github runners at the repository level."
-    default = ""
-    type = string
+  description = "Sets the name of the repository. This option is only used if you use self-hosted Github runners at the repository level."
+  default     = ""
+  type        = string
 }
 
 variable "github_authentication_user" {
-    description = "Sets the user used for issuing new registration tokens. Ensure that the user has the appropriate permissions. "
-    type = string
+  description = "Sets the user used for issuing new registration tokens. Ensure that the user has the appropriate permissions. "
+  type        = string
 }
 
 variable "github_authentication_token" {
-    description = " Sets the personal access token for the configured user in the variable github_authentication_user."
-    type = string
+  description = " Sets the personal access token for the configured user in the variable github_authentication_user."
+  type        = string
 }
 
 variable "github_runner_type" {
-    description = "Defines the github runner type. Available values are: repo, org"
-    default = "repo"
-    type = string
+  description = "Defines the github runner type. Available values are: repo, org"
+  default     = "repo"
+  type        = string
 }
